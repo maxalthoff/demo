54ca07a5cff950fee5a4e55eed7a826482e51a30
Max Althoff <11089186+maxalthoff@users.noreply.github.com>
2024-10-24 19:40:34 -0500

    more commits!

--- index.php
+++ index.php
@@ -7,8 +7,10 @@ require 'Database.php';
 // Connect to the database and execute a query.
 $config = require 'config.php';
 $db = new Database($config['database']);
-$posts = $db->query("select * from posts")->fetchAll();
 
-foreach ($posts as $post) {
-    echo '<li>' . $post['title'] . '</li>';
-}
+$id = $_GET['id'];
+$query = "select * from posts where id = ?";
+
+$posts = $db->query($query, [$id])->fetch();
+
+dd($posts);
